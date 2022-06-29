23346aa5a08a32207ee70de9ad4e66e62063f363
2234145 <107188520+2234145@users.noreply.github.com>
2022-06-29 21:52:49 +0900

    전사 만들기

---
+++ FirstProject1/Enum_SwitchCase/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+// enum (snumerated type) 열거형
+// enum 의 기본요소는 모두 int 형
+enum PlayerState
+{
+
+}
+
+
+namespace Enum_SwitchCase
+{
+    static bool doAttack;
+    static bool doJump;
+    static bool doWalk;
+    static bool doRun;
+    static bool doDash;
+    static bool doHome;
+
+    static PlayerState initState = PlayerState.Attack;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior warrior = new Warrior();
+            warrior.name = "먼지전사";
+
+            if (doAttck)
+                warrior.Attack();
+            else if (doJump)
+                warrior.Jump();
+            else if
+
+
+
+            switch (initSate)
+            {
+                    default:
+            }
+        }
+    }
+
+
+    class Warrior
+    {
+        public string name;
+
+        public void Attack()
+        {
+
+        }
+
+
+    }
+
+}
--- FirstProject1/GenericCollections/Program.cs
+++ FirstProject1/GenericCollections/Program.cs
@@ -16,6 +16,15 @@ namespace GenericCollections
     {
         static void Main(string[] args)
         {
+            // 박싱: 자료형을 객체타입으로 변환하는 과정
+            // 언박싱: 객체타입을 원래 자료형으로 변환하는 과정
+            // System.Collections를 더이상 사용하지 않는 이유는
+            //
+            System.Collections.ArrayList arrList = new System.Collections.ArrayList();
+            arrList.Add(3);
+            arrList.Add("철수");
+            arrList.Add('A');
+
             // List
             List<int> list_int = new List<int>();
             List< float> list_float = new List<float>();
@@ -35,23 +44,77 @@ namespace GenericCollections
             // list_int.Find(x => x == 0);
             list_int.Contains(0);
 
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(list_int[i]);
+            }
+
 
            // LinkedList
            LinkedList<int> linkedList = new LinkedList<int>();
+
+           // 추가
            linkedList.AddLast(0);
            linkedList.AddFirst(1);
            linkedList.AddBefore(linkedList.Find(0), 3);
            Console.WriteLine(linkedList.First);
-            linkedList = new LinkedList<int>();
+           
+            // 삭제
             linkedList.RemoveLast();
+
+            // Hashtable ( 박싱/ 언박싱 때문에 쓰지않음)
+            //--------------------------------------------
+            System.Collections.Hashtable ht = new System.Collections.Hashtable();
+            ht.Add("철수", 90);
 
 
 
            // Dictionary
+           Dictionary<string, char> grades = new Dictionary<string, char>();
 
-           // Queue
+
+           // 추가
+           grades.Add("철수", 'A');
+           grades.Add("영희", 'B');
+
+           // 삭제
+           grades.Remove("철수");
+
+           
+           if (grades.TryAdd("영희", 'C'))
+           {
+                Console.WriteLine("영희 점수 C 추가");
+           }
+           else
+           {
+                Console.WriteLine($"영희 점수 이미 있음 : {grades["영희"]}");
+           }
+           foreach (var sub in grades)
+           {
+                Console.WriteLine(sub);
+                Console.WriteLine(grades[sub]);
+           }
+
+           foreach (var sub in grades.Values)
+           {
+                Console.WriteLine(grades[sub]);
+           }
+
+            // Queue
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Dequeue(); // queue 제일 앞에있는 아이템 반환 및 제거
+            Console.WriteLine(queue.Dequeue());
+            queue.Peek(); // 제일 앞에있는 아이템 반환
+            queue.TryPeek(out int peek);
 
            // Stack
+           Stack<int> stack = new Stack<int>();
+           stack.Push(peek);
+           
         }
     }
 }
