1497a1e4a00acac66afcb3cd93b156868fad8133
2234145 <107188520+2234145@users.noreply.github.com>
2022-06-30 22:00:44 +0900

    말 경주하기
    재미있다

---
+++ FirstProject1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+//프로그램 시작시
+//말 다섯마리를 만들고
+//각 다섯마리는 초당 10~20 (정수형) 범위 거리를 랜덤하게 움직임
+//각각의 말이 거리 200 에 도달하면 말의 이름과 등수를 출력해줌
+
+//말은
+//이름, 달린거리 를 멤버변수로
+//달리기 를 멤버 함수로 가짐.
+//달리기 멤버함수는 입력받은 거리를 달린거리에 더해주어서 달린거리를 누적시키는 역할을 함.
+
+//매초 달릴 때 마다 각 말들이 얼마나 거리를 이동했는지 콘솔창에 출력해줌.
+//경주가 끝나면 1,2,3,4,5 등 말의 이름을 등수 순서대로 콘솔창에 출력해줌.
+
+// - Hint -
+
+//System.Threading namespace 에 있는 Thread.Sleep(1000); 를 사용하면 현재 프로그램을 1초 지연시킬수 있음
+//While 반복문에서 Thread.Sleep(1000); 을 추가하면 1초에 한번씩 반복문을 실행함.
+
+namespace Example4_HorseRacing
+{
+    internal class Program
+    {
+        static Random random;
+        static bool isGameFinished = false;
+        static int minSpeed = 10;
+        static int maxSpeed = 20;
+        static int finishedDistance = 200;
+
+        static void Main(string[] args)
+        {
+            // 말 다섯마리 만들고
+            // 각각의 말 이름 정해주고
+
+            Horse[] arr_Horse = new Horse[5];
+            string[] arr_FinishedHorseName = new string[5];
+            int currentGrade = 1;
+
+            int length = arr_Horse.Length;
+            for (int i = 0; i < length; i++)
+            {
+                arr_Horse[i] = new Horse($"{i} 번마");
+                arr_Horse[i].name = $"경주마{i + 1}";
+            }
+            Console.WriteLine("경주시작!");
+            int 
+            //경주중
+            while (true) 
+            {
+                 
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+    public class Horse
+    {
+        public string name;
+        public int distance;
+        public bool enabled;
+
+        public void Run(int mobeDistance)
+        {
+            distance += mobeDistance;
+        }
+    }
+}
--- FirstProject1/Enum_SwitchCase/Program.cs
+++ FirstProject1/Enum_SwitchCase/Program.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 // enum (snumerated type) 열거형
 // enum 의 기본요소는 모두 int 형
+// enum 의 기본형은 한번에 하나의 요소만 표현가능하다
 enum PlayerState
 {
-
+    Idle,    // ... 00000000
+    Attack,  // ... 00000001
+    Jump,    // ... 00000010
+    Walk,    // ... 00000011
+    Run,     // ... 00000100
+    Dash,    // ... 00000101
+    Home,    // ... 00000110
 }
 
+// Flags Attribute
+// Tostring()에서 명시되지 않은 enum 요소에 대해서 다른 요소들로 구성되는 문자열로 변환해줄 수 있는 속성
+// Attribute 
+[Flags]
+enum PlayerStateFlags
+{
+    Idle = 0 << 0,   // ... 00000000
+    Attack = 1 << 0, // ... 00000001
+    Jump = 1 << 1,   // ... 00000010
+    Walk = 1 << 2,   // ... 00000100
+    Run = 1 << 3,    // ... 00001000
+    Dash = 1 << 4,   // ... 00010000
+    Home = 1 << 5,   // ... 00100000
+}
 
 namespace Enum_SwitchCase
 {
@@ -17,11 +38,16 @@ namespace Enum_SwitchCase
     static bool doHome;
 
     static PlayerState initState = PlayerState.Attack;
+    static PlayerState flags = PlayerState.Attack | PlayerState.Jump;
+
+    
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(flags);
+
             Warrior warrior = new Warrior();
             warrior.name = "먼지전사";
 
@@ -33,9 +59,42 @@ namespace Enum_SwitchCase
 
 
 
-            switch (initSate)
+            switch (initState)
             {
-                    default:
+                   case PlayerState.Idle:
+                        break;
+                   case PlayerState.Attack:
+                        warrior.Attack();
+                        break;
+                   case PlayerState.Jump:
+                        warrior.Jump();
+                        break;
+                   case PlayerState.Walk:
+                        warrior.Walk();
+                        break;
+                   case PlayerState.Run:
+                        warrior.Run();
+                        break;
+                   case PlayerState.Dash:
+                        warrior.Dash();
+                        break;
+                   case PlayerState.Home:
+                        break;
+                   default:
+                        break;
+            }
+
+            // 동작명령
+            Console.WriteLine("전사에게 명령을 내려주세요");
+            string order = Console.ReadLine();
+
+            if (Enum.TryParse(order, out PlayerState orderState));
+            {
+                switch (orderState);
+            }
+            else
+            {
+                Console.WriteLine("동작이 올바르지 않습니다.");
             }
         }
     }
@@ -47,7 +106,7 @@ namespace Enum_SwitchCase
 
         public void Attack()
         {
-
+            Console.WriteLine($"{name}"가 공격함)
         }
 
 
