2d36fd3ad508dc94af4515513e9db5567f4e4ba3
2234145 <107188520+2234145@users.noreply.github.com>
2022-07-05 21:42:08 +0900

    delegate

---
+++ FirstProject1/Delegate/IOP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class IOP
+    {
+        public delegate void OPDel(int a, int b);
+
+        public OPDel Del { get; set; }
+        public event OPDel DelEvent;
+    }
+
+
+    public class Op : IOP
+    {
+        public Op Del { get => throw new NotImplementedException(); set }
+}
---
+++ FirstProject1/Delegate/OPs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class OPs
+    {
+        public enum OP
+        {
+            SUM,
+            SUB,
+            MUL,
+            DIV,
+            MOD,
+        }
+
+        public static int DoOP()
+        {
+
+        }
+
+        static bool RefreshOP(OP op, int a, int b)
+        {
+            switch (op)
+            {
+                case OP.SUM:
+                    result = Sum(a, b);
+                case OP.SUB:
+                    result = Sub(a, b);
+                    break;
+                case OP.MUL:
+                    result = Mul(a, b);
+                    break;
+                case OP.DIV:
+                    result = Div(a, b);
+                    break;
+                case OP.MOD:
+                    result = Mod(a, b);
+                    break;
+            }
+        }
+        static int Sum(int a, int b)
+        {
+
+        }
+
+        static int Sub(int a, int b) => a - b;
+
+        static int Mul(int a, int b) => a * b;
+
+        static int Div(int a, int b) => a / b;
+
+        static int Mod(int a, int b) => a % b;
+    }
+}
