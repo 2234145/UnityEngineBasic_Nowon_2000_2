af95eeb8d74a30290c043508d281b56e73166bf8
Dokyeong <dlgnldhs0926@naver.com>
2022-09-15 21:47:43 +0900

    Tower Defence
    Tower Level

--- TowerDefence/Assets/02.Scripts/TowerHandler.cs
+++ TowerDefence/Assets/02.Scripts/TowerHandler.cs
@@ -1,8 +1,10 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TowerHandler : MonoBehaviour
+public class TowerHandler : MonoBehaviour, IPointerClickHandler
 {
     public static TowerHandler instance;
 
@@ -30,12 +32,14 @@ public class TowerHandler : MonoBehaviour
             throw new System.Exception("고스트 타워 참조 실패");
         }
 
-        
-
     }
+
 
     public void Clear()
     {
+        if (_ghostTower != null)
+            Destroy(_ghostTower);
+
         _ghostTower = null;
         _selectedTowerInfo=null;
         gameObject.SetActive(false);
@@ -59,20 +63,55 @@ public class TowerHandler : MonoBehaviour
 
     private void Update()
     {
-        if (_ghostTower != null)
-            Destroy (_ghostTower);
+        if (_ghostTower == null)
+            return;
 
         _ray = _camera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(_ray, out _hit, Mathf. Infinity, _nodeLayer ))
         {
-          SetGhostTowerPosition ( _hit.collider.transform.position);
+          SetGhostTowerPosition ( _hit.collider.transform.position + Vector3.up *0.5f);
+            _ghostTower.SetActive(true);
         }
         else
         {
             _ghostTower.SetActive(false);
         }
+
+         if (Input.GetKey(KeyCode.Escape))
+            Clear();
+
+        transform.position = Input.mousePosition;
     }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_hit.collider == null)
+            return;
+
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            buildTower();
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Clear();
+        }
+
+    }
+
+    private void buildTower()
+    {
+        if (_selectedTowerInfo.buildPrice > Player.instance.money)
+        {
+            Debug.Log($"잔액이 부족합니다");
+            return;
+        }
 
 
+        if (_hit.collider.GetComponent<Node>().TryBuildTowerHere(_selectedTowerInfo.name, _hit.collider.GetComponent<Node>().GetUp()))
+        {
+            Debug.Log($"타워 건설 완료{_selectedTowerInfo.name}");
+            Player.instance.money -= _selectedTowerInfo.buildPrice; //돈 차감
+        }
+    }
 }
